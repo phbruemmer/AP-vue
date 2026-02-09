b36e3e2a5dfa176d4cc20a856676f36f82c2ba5f
phbruemmer <phbruemmer@gmail.com>
2026-02-09 01:25:04 +0100

    - LargeBlock is currently work in progress. (I have to figure out how I want to style it)

---
+++ src/components/TextBoxes/LargeBlockBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-container">
+    <div class="title"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string;
+  subtitle: string;
+}>();
+</script>
+
+<style scoped></style>
---
+++ src/components/templates/AppointmentCTA.vue
@@ -0,0 +1,13 @@
+<template>
+  <LargeBlockBox title="Titel" subtitle="Sub-Titel">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam explicabo
+    dolores placeat harum reprehenderit, a praesentium veniam animi incidunt
+    quos atque ea laudantium error iste at porro soluta. Tenetur, tempora.
+  </LargeBlockBox>
+</template>
+
+<script lang="ts" setup>
+import LargeBlockBox from "../TextBoxes/LargeBlockBox.vue";
+</script>
+
+<style scoped></style>
