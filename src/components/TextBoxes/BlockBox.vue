217e34b57cc6076df540d8770cf01c13d9de6726
phbruemmer <phbruemmer@gmail.com>
2026-02-09 16:22:59 +0100

    - Fixed BlockBox positioning (uncentered and not responsive)

--- src/components/TextBoxes/BlockBox.vue
+++ src/components/TextBoxes/BlockBox.vue
@@ -1,39 +1,57 @@
 <template>
-  <div class="block">
-    <h3 class="title">{{ props.title }}</h3>
-    <p class="content">
-      <slot></slot>
-    </p>
+  <div class="main">
+    <div class="block">
+      <h3 class="title">{{ title }}</h3>
+      <p class="content">
+        <slot />
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{
+defineProps<{
   title: string;
 }>();
 </script>
 
 <style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+}
+
 .block {
-  width: 40vw;
+  width: 80%;
   max-width: 600px;
-  border-radius: 16px;
 
   text-align: left;
-  background: #f2f2f293;
-  padding: 0 24px;
 
-  cursor: pointer;
+  padding: 20px 20px;
+  border-radius: 14px;
+  background: rgba(0, 0, 0, 0.025);
+
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.03);
   transition: all 0.2s ease;
 }
 
 .block:hover {
-  transform: translateY(-3px);
+  box-shadow: 0 8px 24px rgba(0, 0, 0, 0.06);
+  transform: translateY(-2px);
 }
 
 .title {
+  margin-bottom: 8px;
+
   font-family: "Bebas";
-  letter-spacing: 2px;
-  font-weight: 100;
+  font-size: 1.25rem;
+  letter-spacing: 1.5px;
+  font-weight: 400;
+}
+
+.content {
+  margin: 0;
+  line-height: 1.6;
+  color: #444;
 }
 </style>
