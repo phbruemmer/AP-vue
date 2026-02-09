737ff104da407f7d6f2aede9c7bef50a3b339378
phbruemmer <phbruemmer@gmail.com>
2026-02-09 03:37:40 +0100

    - Detached navigation bar is now work in progress

---
+++ src/components/DetachedNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navigation">
+    <p>placeholder</p>
+    <p>placeholder</p>
+    <p>placeholder</p>
+    <p>placeholder</p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.navigation {
+}
+</style>
---
+++ src/components/templates/NavigationBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <DetachedNavbar> </DetachedNavbar>
+</template>
+
+<script lang="ts" setup>
+import DetachedNavbar from "../DetachedNavbar.vue";
+</script>
+
+<style scoped></style>
