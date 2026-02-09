6c57a71742c067f25b36c48f7acf0b8dd93cf88e
phbruemmer <phbruemmer@gmail.com>
2026-02-09 21:55:40 +0100

    - Added expand functionality to the service section to improve mobile ui / ux

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -24,28 +24,52 @@
       </BlockBox>
 
       <!-- Fourth Box -->
-      <BlockBox title="Titel">
+      <BlockBox title="Titel" v-if="(isMobile && contentLoaded) || !isMobile">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
         dignissimos necessitatibus.
       </BlockBox>
 
       <!-- Fifth Box -->
-      <BlockBox title="Titel">
+      <BlockBox title="Titel" v-if="(isMobile && contentLoaded) || !isMobile">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
         dignissimos necessitatibus.
       </BlockBox>
 
       <!-- Sixth Box -->
-      <BlockBox title="Titel">
+      <BlockBox title="Titel" v-if="(isMobile && contentLoaded) || !isMobile">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
         dignissimos necessitatibus.
       </BlockBox>
     </div>
+
+    <p
+      class="load-more"
+      @click="contentLoaded = true"
+      v-if="isMobile && !contentLoaded"
+    >
+      Mehr laden
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, onUnmounted, ref } from "vue";
 import BlockBox from "./TextBoxes/BlockBox.vue";
+
+const isMobile = ref<boolean>(false);
+const contentLoaded = ref<boolean>(false);
+
+const checkSize = () => {
+  isMobile.value = window.innerWidth <= 896;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", checkSize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", checkSize);
+});
 </script>
 
 <style scoped>
@@ -65,6 +89,7 @@ h2 {
 .content-container {
   display: flex;
   justify-content: center;
+  flex-direction: column;
   margin: 0 2vw;
 }
 
@@ -75,6 +100,12 @@ h2 {
 
   max-width: 1200px;
   margin: 0 auto;
+}
+
+.load-more {
+  margin-top: 4vh;
+  text-decoration: underline;
+  cursor: pointer;
 }
 
 @media (max-width: 896px) {
