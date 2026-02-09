65dc4dee0e365729dbc15213419c571f794db9f2
phbruemmer <phbruemmer@gmail.com>
2026-02-09 01:23:37 +0100

    - Changed Service.vue box styling

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,34 +3,50 @@
     <h2>Leistungen</h2>
   </div>
 
-  <div class="content-wrapper">
-    <div class="block">
-      <h3 class="title">Service 1</h3>
-      <p class="content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-        harum esse temporibus ea nesciunt modi commodi.
-      </p>
-    </div>
+  <div class="content-container">
+    <div class="content-wrapper">
+      <!-- First Box -->
+      <BlockBox title="Titel">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
+        dignissimos necessitatibus.
+      </BlockBox>
 
-    <div class="block">
-      <h3 class="title">Service 2</h3>
-      <p class="content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta eveniet
-        aliquid quisquam neque porro reprehenderit.
-      </p>
-    </div>
+      <!-- Second Box -->
+      <BlockBox title="Titel">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
+        dignissimos necessitatibus.
+      </BlockBox>
 
-    <div class="block">
-      <h3 class="title">Service 3</h3>
-      <p class="content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
-        harum esse temporibus ea nesciunt modi commodi.
-      </p>
+      <!-- Third Box -->
+      <BlockBox title="Titel">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
+        dignissimos necessitatibus.
+      </BlockBox>
+
+      <!-- Fourth Box -->
+      <BlockBox title="Titel">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
+        dignissimos necessitatibus.
+      </BlockBox>
+
+      <!-- Fifth Box -->
+      <BlockBox title="Titel">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
+        dignissimos necessitatibus.
+      </BlockBox>
+
+      <!-- Sixth Box -->
+      <BlockBox title="Titel">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum a at
+        dignissimos necessitatibus.
+      </BlockBox>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import BlockBox from "./TextBoxes/BlockBox.vue";
+</script>
 
 <style scoped>
 h2 {
@@ -46,39 +62,14 @@ h2 {
   font-weight: 500;
 }
 
-.content-wrapper {
+.content-container {
   display: flex;
   justify-content: center;
-  width: 100vw;
-  gap: 4rem;
-  flex-wrap: wrap;
 }
 
-.block {
-  text-align: center;
-  max-width: 280px;
-  padding: 15px;
-  border-radius: 2px;
-  transition: all 0.2s ease;
-}
-
-.block:hover {
-  transform: translateY(-3px);
-  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.048);
-}
-
-.block .title {
-  font-family: "Bebas";
-  font-size: 1.2rem;
-  font-weight: 600;
-  color: #444;
-  margin-bottom: 0.5rem;
-}
-
-.block .content {
-  font-size: 0.95rem;
-  color: #666;
-  line-height: 1.5;
-  margin: 0;
+.content-wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2vh 2vw;
 }
 </style>
---
+++ src/components/TextBoxes/BlockBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="block">
+    <h3 class="title">{{ props.title }}</h3>
+    <p class="content">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<style scoped>
+.block {
+  width: 40vw;
+  max-width: 600px;
+  border-radius: 16px;
+
+  text-align: left;
+  background: #f2f2f293;
+  padding: 0 24px;
+
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+
+.block:hover {
+  transform: translateY(-3px);
+}
+
+.title {
+  font-family: "Bebas";
+  letter-spacing: 2px;
+  font-weight: 100;
+}
+</style>
