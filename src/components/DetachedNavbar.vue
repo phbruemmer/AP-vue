76266ad1a51216fd74b5bf09d0deabe1a87f9cb2
phbruemmer <phbruemmer@gmail.com>
2026-02-10 21:59:50 +0100

    - improved navigation bar style

--- src/components/DetachedNavbar.vue
+++ src/components/DetachedNavbar.vue
@@ -11,5 +11,20 @@
 
 <style scoped>
 .navigation {
+  position: fixed;
+  top: 2vh;
+  bottom: 0;
+
+  height: 8vh;
+  padding: 0 2vw;
+  border-radius: 50px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 4vw;
+  background: #f5f5fe;
 }
 </style>
