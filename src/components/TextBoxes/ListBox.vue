0069c125bdaa6d1a5421a43af05c9d85739ed816
phbruemmer <phbruemmer@gmail.com>
2026-02-10 19:18:15 +0100

    - Finished ListBoxes and created ExpandableContent template

---
+++ src/components/TextBoxes/ListBox.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="box-container" @click="toggleBox()">
+    <div class="main">
+      <h2>Some random question?</h2>
+
+      <div class="arrow-container" :class="expanded ? 'expanded' : ''">
+        <div class="arrow"></div>
+      </div>
+    </div>
+
+    <transition name="fade-slide">
+      <div class="content-container" v-if="expanded">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const props = defineProps<{
+  title: string;
+}>();
+
+const expanded = ref<boolean>(false);
+
+const toggleBox = () => {
+  expanded.value = !expanded.value;
+};
+</script>
+
+<style scoped>
+.box-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: left;
+  transition: all 0.3s ease;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 2vw;
+}
+
+.main h2 {
+  font-family: "Tasa";
+}
+
+.arrow-container {
+  padding: 12px;
+  cursor: pointer;
+
+  transition: all 0.1s ease;
+}
+
+.arrow-container.expanded {
+  transform: rotate(-90deg);
+}
+
+.arrow {
+  width: 10px;
+  height: 10px;
+  border-bottom: 2px solid #000;
+  border-left: 2px solid #000;
+  transform: rotate(45deg);
+}
+
+.content-container {
+  margin-left: 6vw;
+  width: 20vw;
+  min-width: 250px;
+  max-width: 450px;
+}
+
+.fade-slide-enter-active,
+.fade-slide-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-slide-enter-from,
+.fade-slide-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+.fade-slide-enter-to,
+.fade-slide-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
---
+++ src/components/templates/ExpandableContent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main-container">
+    <h1>Weitere Informationen</h1>
+
+    <ListBox title="Big Title for this">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus porro
+      asperiores quaerat similique totam natus error. Voluptatum maiores
+      reprehenderit vel doloremque ab error, quasi aspernatur recusandae nam
+      nesciunt. Ad, saepe.
+    </ListBox>
+
+    <ListBox title="Big Title for this">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus porro
+      asperiores quaerat similique totam natus error. Voluptatum maiores
+      reprehenderit vel doloremque ab error, quasi aspernatur recusandae nam
+      nesciunt. Ad, saepe.
+    </ListBox>
+
+    <ListBox title="Big Title for this">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus porro
+      asperiores quaerat similique totam natus error. Voluptatum maiores
+      reprehenderit vel doloremque ab error, quasi aspernatur recusandae nam
+      nesciunt. Ad, saepe.
+    </ListBox>
+
+    <ListBox title="Big Title for this">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus porro
+      asperiores quaerat similique totam natus error. Voluptatum maiores
+      reprehenderit vel doloremque ab error, quasi aspernatur recusandae nam
+      nesciunt. Ad, saepe.
+    </ListBox>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ListBox from "../TextBoxes/ListBox.vue";
+</script>
+
+<style scoped>
+.main-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 2vh;
+}
+
+.main-container h1 {
+  font-family: "Tasa-Bold";
+  color: #222;
+}
+</style>
