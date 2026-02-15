3dbc19576c540fdc6981199093ede60310ab643c
phbruemmer <phbruemmer@gmail.com>
2026-02-15 18:21:19 +0100

    - Separating parts of StickySectionNotes.vue into multiple components for easier and better development

---
+++ src/components/StickyNotesComponents/RightContainer.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="right">
+    <div
+      class="cta-element"
+      :class="{
+        first: props.active == 'first',
+        second: props.active == 'second',
+        third: props.active == 'third',
+        fourth: props.active == 'fourth',
+      }"
+    >
+      <Transition name="cta">
+        <div
+          v-if="props.active === 'first'"
+          key="first"
+          class="cta-content first-content"
+        />
+      </Transition>
+
+      <Transition name="cta">
+        <div
+          v-if="props.active === 'second'"
+          key="second"
+          class="cta-content second-content"
+        />
+      </Transition>
+
+      <Transition name="cta">
+        <div
+          v-if="props.active === 'third'"
+          key="third"
+          class="cta-content third-content"
+        />
+      </Transition>
+
+      <Transition name="cta">
+        <div
+          v-if="props.active === 'fourth'"
+          key="fourth"
+          class="cta-content fourth-content"
+        />
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  active: null | "first" | "second" | "third" | "fourth";
+}>();
+</script>
+
+<style scoped>
+.right {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.cta-element {
+  width: clamp(18rem, 30vw, 30rem);
+  aspect-ratio: 1 / 1;
+
+  border-radius: 50% 50% 2px 50%;
+  position: sticky;
+  top: 25%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: all 0.3s ease;
+}
+
+.cta-content {
+  position: absolute;
+
+  width: 80%;
+  height: 60%;
+  border-radius: 16px;
+
+  background: #ffffff;
+  box-shadow: 0 4px 4px #00000011;
+
+  transform: translateX(-50%);
+}
+
+/* Bubble Colors */
+.first {
+  background-color: #b888fa;
+  box-shadow: 0 4px 4px #b788fa23;
+}
+
+.second {
+  background-color: #276fbf;
+  box-shadow: 0 4px 4px #276ebf23;
+}
+
+.third {
+  background-color: #d64550;
+  box-shadow: 0 4px 4px #d645512a;
+}
+
+.fourth {
+  background-color: #f1c40f;
+  box-shadow: 0 4px 4px #f1c40f29;
+}
+
+/* CTA-Content style */
+.first-content {
+  background-image: url("https://wallpapercave.com/wp/wp4854991.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.second-content {
+  background-image: url("https://www.pixelstalk.net/wp-content/uploads/2016/12/Free-Beautiful-Landscape-Image.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.third-content {
+  background-image: url("https://th.bing.com/th/id/R.91b787ec89a49140e9c73be53271a03b?rik=Yjdehc0JKgaohA&pid=ImgRaw&r=0");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.fourth-content {
+  background-image: url("https://img1.akspic.ru/crops/4/0/3/2/22304/22304-otrazhenie-pejzazhi_gor-priroda-prirodnyj_zapovednik-dikaya_mestnost-1920x1080.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+/* CTA-Content animation */
+.cta-enter-active,
+.cta-leave-active {
+  transition: all 0.3s ease;
+}
+
+.cta-enter-from,
+.cta-leave-to {
+  opacity: 0;
+  transform: translateX(0) scale(50%);
+}
+
+.cta-enter-to,
+.cta-leave-from {
+  opacity: 1;
+  transform: translateX(-50%) scale(1);
+}
+</style>
--- src/components/StickySectionNotes.vue
+++ src/components/StickySectionNotes.vue
@@ -72,49 +72,7 @@
           </transition>
         </div>
       </div>
-      <div class="right">
-        <div
-          class="cta-element"
-          :class="{
-            first: active == 'first',
-            second: active == 'second',
-            third: active == 'third',
-            fourth: active == 'fourth',
-          }"
-        >
-          <Transition name="cta">
-            <div
-              v-if="active === 'first'"
-              key="first"
-              class="cta-content first-content"
-            />
-          </Transition>
-
-          <Transition name="cta">
-            <div
-              v-if="active === 'second'"
-              key="second"
-              class="cta-content second-content"
-            />
-          </Transition>
-
-          <Transition name="cta">
-            <div
-              v-if="active === 'third'"
-              key="third"
-              class="cta-content third-content"
-            />
-          </Transition>
-
-          <Transition name="cta">
-            <div
-              v-if="active === 'fourth'"
-              key="fourth"
-              class="cta-content fourth-content"
-            />
-          </Transition>
-        </div>
-      </div>
+      <RightContainer :active="active" />
     </div>
 
     <div class="lower-center" id="calendar">
@@ -128,17 +86,17 @@
 </template>
 
 <script lang="ts" setup>
-import { useElementVisibility } from "@vueuse/core";
 import { onMounted, onUnmounted, ref, useTemplateRef, watch } from "vue";
+import { useElementVisibility } from "@vueuse/core";
 
-import Redirection from "./Navigation/Redirection.vue";
 import StandardTextBox from "./TextBoxes/StandardTextBox.vue";
 import Calendar from "./Calendar.vue";
+import RightContainer from "./StickyNotesComponents/RightContainer.vue";
+
+const isMobile = ref<boolean>(false);
 
 type Active = null | "first" | "second" | "third" | "fourth";
 const active = ref<Active>(null);
-
-const isMobile = ref<boolean>(false);
 
 const firstTarget = useTemplateRef("firstTarget");
 const firstTargetVisible = useElementVisibility(firstTarget, {
@@ -272,7 +230,6 @@ onUnmounted(() => {
 
   cursor: pointer;
   transition: all 0.1s ease;
-  animation: gradientMove 6s linear infinite;
 }
 
 .cta-btn:active {
@@ -288,6 +245,7 @@ onUnmounted(() => {
 
 .center {
   min-height: 100vh;
+  max-width: 100%;
   background: #5389b90e;
 
   display: grid;
@@ -302,38 +260,25 @@ onUnmounted(() => {
   padding: 7.5rem 0;
 }
 
-.right {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
+/* Bubble Colors */
+.first {
+  background-color: #b888fa;
+  box-shadow: 0 4px 4px #b788fa23;
 }
 
-.cta-element {
-  width: clamp(18rem, 30vw, 30rem);
-  aspect-ratio: 1 / 1;
-
-  border-radius: 50% 50% 2px 50%;
-  position: sticky;
-  top: 25%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  transition: all 0.3s ease;
+.second {
+  background-color: #276fbf;
+  box-shadow: 0 4px 4px #276ebf23;
 }
 
-.cta-content {
-  position: absolute;
+.third {
+  background-color: #d64550;
+  box-shadow: 0 4px 4px #d645512a;
+}
 
-  width: 80%;
-  height: 60%;
-  border-radius: 16px;
-
-  background: #ffffff;
-  box-shadow: 0 4px 4px #00000011;
-
-  transform: translateX(-50%);
+.fourth {
+  background-color: #f1c40f;
+  box-shadow: 0 4px 4px #f1c40f29;
 }
 
 /* Mobile CTA */
@@ -389,70 +334,6 @@ onUnmounted(() => {
   clip-path: ellipse(100% 100% at 50% 0%);
   position: relative;
   z-index: 9999;
-}
-
-/* Bubble Colors */
-.first {
-  background-color: #b888fa;
-  box-shadow: 0 4px 4px #b788fa23;
-}
-
-.second {
-  background-color: #276fbf;
-  box-shadow: 0 4px 4px #276ebf23;
-}
-
-.third {
-  background-color: #d64550;
-  box-shadow: 0 4px 4px #d645512a;
-}
-
-.fourth {
-  background-color: #f1c40f;
-  box-shadow: 0 4px 4px #f1c40f29;
-}
-
-/* CTA-Content style */
-.first-content {
-  background-image: url("https://wallpapercave.com/wp/wp4854991.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.second-content {
-  background-image: url("https://www.pixelstalk.net/wp-content/uploads/2016/12/Free-Beautiful-Landscape-Image.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.third-content {
-  background-image: url("https://th.bing.com/th/id/R.91b787ec89a49140e9c73be53271a03b?rik=Yjdehc0JKgaohA&pid=ImgRaw&r=0");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-.fourth-content {
-  background-image: url("https://img1.akspic.ru/crops/4/0/3/2/22304/22304-otrazhenie-pejzazhi_gor-priroda-prirodnyj_zapovednik-dikaya_mestnost-1920x1080.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-/* CTA-Content animation */
-.cta-enter-active,
-.cta-leave-active {
-  transition: all 0.3s ease;
-}
-
-.cta-enter-from,
-.cta-leave-to {
-  opacity: 0;
-  transform: translateX(0) scale(50%);
-}
-
-.cta-enter-to,
-.cta-leave-from {
-  opacity: 1;
-  transform: translateX(-50%) scale(1);
 }
 
 /* Appear Animation */
